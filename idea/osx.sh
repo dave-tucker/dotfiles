225c909c8faa7884da00fd40189bb9a0751651f3
Dave Tucker <dave@dtucker.co.uk>
2014-02-25 00:31:58 +0000

    Updated Idea install script

--- idea/osx.sh
+++ idea/osx.sh
@@ -3,5 +3,4 @@
 cd "$(dirname "$0")"
 
 echo "  Installing IntelliJ Colors"
-mkdir -p ~/Library/Application\ Support/IdeaIC13/colors
-cp *.icls ~/Library/Application\ Support/IdeaIC13/colors
+cp *.icls ~/Library/Preferences/IdeaIC13/colors
