47a05910363d1e1b2831ae1542ca09a68db775e0
Dave Tucker <dave@dtucker.co.uk>
2014-06-10 16:28:12 +0100

    Add more vagrant aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,19 @@
 #aliases go here
 # Vagrant
-alias vgu=vagrant up
-alias vgd=vagrant destroy
-alias vgr=vagrant reload
-alias vgh=vagrant halt
-alias vgs=vagrant ssh
+alias vgu='vagrant up'
+alias vgd='vagrant destroy'
+alias vgr='vagrant reload'
+alias vgh='vagrant halt'
+alias vgs='vagrant ssh'
+alias vgst = 'vagrant status'
+alias vgsu = 'vagrant suspend'
+alias vgsh = 'vagrant share'
+alias vgl = 'vagrant login'
+alias vgp = 'vagrant provision'
+alias vgi = 'vagrant init'
+alias vgpi = 'vagrant plugin'
+alias vgpkg = 'vagrant package'
+alias vggs = 'vagrant global-status'
+alias vgc = 'vagrant connect'
+alias vgrdp = 'vagrant rdp'
+
