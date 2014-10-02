a12e73eef19f1c7a532c4bbe09251ca3e7009acd
Jeff Widman <jeff@jeffwidman.com>
2014-10-01 23:11:55 -0700

    Bumped from Sublime 2 to Sublime 3

--- sublime/linux.sh
+++ sublime/linux.sh
@@ -2,6 +2,6 @@
 
 cd "$(dirname "$0")"
 
-echo "   Installing Sublime Text 2 Preferences"
+echo "   Installing Sublime Text 3 Preferences"
 
-cp -v User/* ~/.config/sublime-text-2/Packages/User/
+cp -v User/* ~/.config/sublime-text-3/Packages/User/
