f12f2465841a96f737cae79cffaa255f0fcc7b55
Dave Tucker <dave@dtucker.co.uk>
2026-05-14 16:11:53 +0100

    autoCommit and autoPush, rust-analyzer, tools
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -516,7 +516,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
